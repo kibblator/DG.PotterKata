4a6667c2346085df80124b3c7f76e255d18d3ac4
Daniel Gregson <daniel.gregson@ao.com>
2019-12-04 16:09:42 +0000

    Initial file setup with test project

---
+++ tests/DG.PotterKata.Tests/PotterKataTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DG.PotterKata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
