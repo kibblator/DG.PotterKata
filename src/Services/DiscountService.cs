898128ce63425053c881352714885f9fa61aa661
Daniel Gregson <kornfreakuk@hotmail.com>
2019-12-05 09:47:28 +0000

    Oops, added 4 to the dictionary twice instead of 4 and 5!

--- src/Services/DiscountService.cs
+++ src/Services/DiscountService.cs
@@ -6,18 +6,18 @@ namespace DG.PotterKata.Services
 {
     public static class DiscountService
     {
-        private static Dictionary<int, decimal> discountAmounts = new Dictionary<int, decimal>
+        private static readonly Dictionary<int, decimal> DiscountAmounts = new Dictionary<int, decimal>
         {
             {1, 1},
             {2, 0.95m},
             {3, 0.9m},
             {4, 0.8m},
-            {4, 0.75m}
+            {5, 0.75m}
         };
 
         public static decimal CalcDiscount(IEnumerable<Book> books, decimal subTotal)
         {
-            var total = subTotal * discountAmounts[books.GroupBy(b => b.BookId).Count()];
+            var total = subTotal * DiscountAmounts[books.GroupBy(b => b.BookId).Count()];
 
             return total;
         }
